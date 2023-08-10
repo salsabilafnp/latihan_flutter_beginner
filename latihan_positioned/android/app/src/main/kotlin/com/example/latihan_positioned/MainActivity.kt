7ea2bec9c47fb96ac7223b7baa80ff0d3db52576
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-10 16:13:53 +0700

    latihan - login page

---
+++ latihan_positioned/android/app/src/main/kotlin/com/example/latihan_positioned/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_positioned
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan_positioned/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:latihan_positioned/main_page.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
