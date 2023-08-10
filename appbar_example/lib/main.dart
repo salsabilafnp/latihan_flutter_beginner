ebfc02c2fbf07056abe12867effe3171e61908f7
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-10 10:56:37 +0700

    latihan explore appbar

---
+++ appbar_example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "AppBar Example",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.exit_to_app)),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+              ),
+              image: DecorationImage(
+                  image: AssetImage("assets/overlay.png"),
+                  fit: BoxFit.none,
+                  repeat: ImageRepeat.repeat),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
