40622d787cd4e9682a4e0f3465ab6ae90232a127
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-10 11:33:41 +0700

    latihan textfield - decoration

--- latihan_textfield/lib/main.dart
+++ latihan_textfield/lib/main.dart
@@ -25,9 +25,40 @@ class _MyAppState extends State<MyApp> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             TextField(
+              decoration: InputDecoration(
+                fillColor: Colors.lightBlue[50],
+                filled: true,
+                // prefix widget
+                // prefix: Container(
+                //   width: 5,
+                //   height: 5,
+                //   color: Colors.red,
+                // ),
+                // icon di luar tf
+                icon: Icon(Icons.person),
+                // suffix widget
+                // suffix: Container(
+                //   width: 5,
+                //   height: 5,
+                //   color: Colors.red,
+                // ),
+                // text di atas tf
+                labelText: "Nama Lengkap",
+                // clue
+                hintText: "Nama Lengkap Anda Tanpa Gelar",
+                // icon di dalam tf
+                // prefixIcon: Icon(Icons.person),
+                // text di dalam tf
+                // prefixText: "Name : ",
+                // style untuk text di dalam tf
+                // prefixStyle:
+                //     TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
               // maxLength: 5,
-              maxLines: 1,
-              obscureText: true,
+              // maxLines: 1,
+              // obscureText: true,
               onChanged: (value) {
                 setState(() {});
               },
