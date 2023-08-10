8ed384885c85b793282ae604c0b9a2b84d916d3c
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-10 10:22:48 +0700

    latihan navigasi

---
+++ latihan_navigation/lib/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_navigation/main_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Login"),
+          onPressed: () {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) {
+              return MainPage();
+            }));
+          },
+        ),
+      ),
+    );
+  }
+}
