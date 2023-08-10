29f58796fff3f5f7a360cb68c4bafc1ccea25663
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-10 15:13:33 +0700

    latihan custom card

---
+++ latihan_custom_card/android/app/src/main/kotlin/com/example/latihan_custom_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_custom_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan_custom_card/lib/main_page.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Custom Card",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8c062f),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xfffe5788), Color(0xfff56d5d)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(
+                elevation: 10,
+                child: Stack(
+                  children: [
+                    Opacity(
+                      opacity: 0.7,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://www.toptal.com/designers/subtlepatterns/uploads/ffd6f82c-60e2-4c97-bdc9-850e966f3ed7/pink-flowers.png"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(4),
+                            topRight: Radius.circular(4),
+                          ),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://pixabay.com/get/gf23989e56e5bb158660cc4422ed4a5ebf82e80f7ace01e944c64e7670acddeeb9d844d1d1ef164045da76f426d49188c_1280.jpg"),
+                              fit: BoxFit.cover)),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(
+                        20,
+                        50 + MediaQuery.of(context).size.height * 0.35,
+                        20,
+                        20,
+                      ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Beautiful Sunset at Park",
+                              maxLines: 2,
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 25),
+                            ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 20, 0, 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Posted on ",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    "August 10, 2023",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                Spacer(
+                                  flex: 10,
+                                ),
+                                Icon(
+                                  Icons.thumb_up,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "99",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 5,
+                                ),
+                                Icon(
+                                  Icons.comment,
+                                  size: 18,
+                                  color: Colors.grey,
+                                ),
+                                Spacer(
+                                  flex: 1,
+                                ),
+                                Text(
+                                  "888",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                Spacer(
+                                  flex: 10,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
