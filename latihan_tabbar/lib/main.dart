2666f33ea434101c80789233d1bb6e92754e878e
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-11 14:03:29 +0700

    latihan tabBar - 2

--- latihan_tabbar/lib/main.dart
+++ latihan_tabbar/lib/main.dart
@@ -7,39 +7,43 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TabBar myTabBar = TabBar(
+      // indicatorColor: Colors.green,
+      indicator: BoxDecoration(
+        color: Colors.red,
+        border: Border(
+          top: BorderSide(
+            color: Colors.purple,
+            width: 5,
+          ),
+        ),
+      ),
+      tabs: [
+        Tab(
+          icon: Icon(Icons.home),
+          text: "Home",
+        ),
+        Tab(
+          icon: Icon(Icons.person),
+          text: "Profile",
+        ),
+      ],
+    );
+
     return MaterialApp(
       home: DefaultTabController(
-        length: 4,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             title: Text("Contoh Tab Bar"),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.home),
-                  text: "Home",
-                ),
-                Tab(
-                  child: Image(
-                    image: NetworkImage(
-                        "https://cdn.shopify.com/s/files/1/1061/1924/files/Hugging_Face_Emoji_2028ce8b-c213-4d45-94aa-21e1a0842b4d_large.png?15202324258887420558"),
-                  ),
-                ),
-                Tab(
-                  icon: Icon(Icons.computer),
-                ),
-                Tab(
-                  text: "News",
-                ),
-              ],
-            ),
+            bottom: PreferredSize(
+                preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
+                child: Container(color: Colors.amber, child: myTabBar)),
           ),
           body: TabBarView(
             children: [
-              Center(child: Text('Halaman Home')),
-              Center(child: Text('Halaman Test')),
-              Center(child: Text('Halaman Computer')),
-              Center(child: Text('Halaman News')),
+              Center(child: Text('Halaman Beranda')),
+              Center(child: Text('Halaman Profil')),
             ],
           ),
         ),
