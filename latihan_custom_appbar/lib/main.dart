e33c1122e95ea063ba8bf0835d176f16d2c1afa8
salsabilafnp <salsabilafebriantynp@gmail.com>
2023-08-11 10:59:41 +0700

    cutom appbar

---
+++ latihan_custom_appbar/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(200),
+          child: AppBar(
+            backgroundColor: Colors.amber,
+            flexibleSpace: Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "AppBar with Custom Height",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
